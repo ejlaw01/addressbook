dbbee2e17d41ff6bdb9bf629e7bb5f4d6483f11b
Ethan Law and Karen Freeman-Smith <karen@>
2016-08-23 09:37:49 -0700

    add functionality for multiple addresses per contact

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,75 @@
 var Contact = function(firstName, lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
+  this.addresses = [];
+}
+
+var Address = function(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
 
 
 // frontend logic
-$("form").submit(function(event){
-  event.preventDefault();
-  var inputFirstName = $("#first-name").val();
-  var inputLastName = $("#last-name").val();
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var inputFirstName = $("#first-name").val();
+    var inputLastName = $("#last-name").val();
 
-  var newContact = new Contact(inputFirstName, inputLastName);
+    var newContact = new Contact(inputFirstName, inputLastName);
 
-  $("ul").append("<li>" + inputFirstName + "</li>");
+debugger;
+    $(".new-address").each(function(){
+      var inputStreet = $(this).find("input.street").val();
+      var inputCity = $(this).find("input.city").val();
+      var inputState = $(this).find("input.state").val();
+      var newAddress = new Address(inputStreet, inputCity, inputState);
+      newContact.addresses.push(newAddress);
+    });
 
-  $("#first-name").val("");
-  $("#last-name").val("");
+    $("ul").append("<li>" + newContact.fullName() + "</li>");
 
-  $("#output li").last().click(function(){
-    $("#show-contact").show();
-    $("#show-contact h2").text(newContact.fullName());
-    $(".first-name").text(newContact.firstName);
-    $(".last-name").text(newContact.lastName);
+    $("#first-name").val("");
+    $("#last-name").val("");
+    $("#street").val("");
+    $("#city").val("");
+    $("#state").val("");
+
+    $("#output li").last().click(function(){
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".address").text("");
+        newContact.addresses.forEach(function(address){
+        $(".address").append("<li>" + address.fullAddress() + "</li>");
+      });
+    });
   });
-
+  $("#new-address").click(function(){
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="street">Street</label>' +
+                                   '<input type="text" class="form-control street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="city">City</label>' +
+                                   '<input type="text" class="form-control city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="state">State</label>' +
+                                   '<input type="text" class="form-control state">' +
+                                 '</div>' +
+                               '</div>');
+  });
 });
